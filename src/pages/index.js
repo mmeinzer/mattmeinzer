19df024edda37da658d624c676bc21606639d9fc
Matt <meinzer.matthew@gmail.com>
2018-06-14 17:32:54 -0500

    Clean up index file by removing old imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import Link from 'gatsby-link'
-
 import styled from 'styled-components'
 
 import PostList from '../components/PostList'
-
 import theme from '../utils/theme'
 
 const Wrapper = styled.section`
