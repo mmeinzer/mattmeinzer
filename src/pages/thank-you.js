3f8292a143d621f9357bd6437f632bf3b3116997
Matt Meinzer <meinzer.matthew@gmail.com>
2020-06-25 17:23:54 -0500

    Tweak email signup thank you page

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -4,12 +4,16 @@ import Layout from '../components/layout'
 
 const Wrapper = styled.div`
   grid-column-start: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
 
 const ThankYouPage = () => (
   <Layout>
     <Wrapper>
-      <h1>Thanks for subscribing!</h1>
+      <h1>Thank you for subscribing!</h1>
+      <p>Please check your email to confirm your subscription</p>
     </Wrapper>
   </Layout>
 )
