b03d44cd7e340c96568cafac10dd45402007ebce
Matt Meinzer <meinzer.matthew@gmail.com>
2020-07-09 22:39:33 -0500

    Run prettier

--- src/pages/dailycast/support.js
+++ src/pages/dailycast/support.js
@@ -1,18 +1,21 @@
-import React from "react";
-import styled from "styled-components";
-import Layout from "../../components/layout";
+import React from 'react'
+import styled from 'styled-components'
+import Layout from '../../components/layout'
 
 const Wrapper = styled.div`
   grid-column-start: center;
-`;
+`
 
 const SupportPage = () => (
   <Layout>
     <Wrapper>
       <h2>Dailycast Support</h2>
-      <p>For any support requests or feedback, please email me at meinzer.matthew@gmail.com.</p>
+      <p>
+        For any support requests or feedback, please email me at
+        meinzer.matthew@gmail.com.
+      </p>
     </Wrapper>
   </Layout>
-);
+)
 
-export default SupportPage;
+export default SupportPage
